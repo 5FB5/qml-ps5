0c036a2b1a2d024bb55a5eb8435afacf80228f10
vzubko <val@zubko01.ru>
2025-09-02 16:52:24 +0300

    tried to handle gamepad in thread but i will rewrite it

--- ps5-screen/backend/gamepad/gamepadmanager.cpp
+++ ps5-screen/backend/gamepad/gamepadmanager.cpp
@@ -3,19 +3,29 @@
 GamepadManager::GamepadManager(QObject *parent)
     : QObject{parent}
 {
-    run();
-}
-
-void GamepadManager::run()
-{
-    int fd = open("/dev/input/js0", O_RDONLY);
+    int fd = open(DEVICE_PATH, O_RDONLY);
 
     if (fd == -1)
     {
-        qWarning() << "[GamepadManager]: Can't open dev/input/js0 device";
+        qWarning() << "[GamepadManager]: Can't open" << DEVICE_PATH << "device";
         return;
     }
 
+    char rawName[100];
+    ioctl(fd, JSIOCGNAME(100), &rawName);
+
+    const QString name = rawName;
+
+    qInfo() << "[GamepadManager]: Found" << name;
+
+    if (name.toLower().contains("xbox"))
+    {
+        future = QtConcurrent::run(handleXboxInput, fd);
+    }
+}
+
+void GamepadManager::handleXboxInput(int fd)
+{
     struct js_event event;
 
     while(readEvent(fd, &event) == 0)
@@ -24,7 +34,7 @@ void GamepadManager::run()
         {
         case JS_EVENT_BUTTON:
         {
-            qDebug() << "Button: " << event.number << (event.value ? "pressed" : "released");
+            qDebug() << XboxButton[event.number];
             break;
         }
         default: break;
@@ -46,4 +56,75 @@ int GamepadManager::readEvent(int fd, js_event *event)
         return 0;
 
     return -1;
+}
+
+void GamepadManager::getButton(uint8_t button, int16_t value)
+{
+    // if (deviceInfo.name.contains("Xbox"))
+    // {
+    //     deviceInfo.buttonInfo.pressed = static_cast<bool>(value);
+    //     switch(button)
+    //     {
+    //     case 0:
+    //     {
+    //         deviceInfo.buttonInfo.name = "A";
+    //         break;
+    //     }
+    //     case 1:
+    //     {
+    //         deviceInfo.buttonInfo.name = "B";
+    //         break;
+    //     }
+    //     case 2:
+    //     {
+    //         deviceInfo.buttonInfo.name = "X";
+    //         break;
+    //     }
+    //     case 3:
+    //     {
+    //         deviceInfo.buttonInfo.name = "Y";
+    //         break;
+    //     }
+    //     case 4:
+    //     {
+    //         deviceInfo.buttonInfo.name = "LB";
+    //         break;
+    //     }
+    //     case 5:
+    //     {
+    //         deviceInfo.buttonInfo.name = "RB";
+    //         break;
+    //     }
+    //     case 6:
+    //     {
+    //         deviceInfo.buttonInfo.name = "Back";
+    //         break;
+    //     }
+    //     case 7:
+    //     {
+    //         deviceInfo.buttonInfo.name = "Start";
+    //         break;
+    //     }
+    //     case 8:
+    //     {
+    //         deviceInfo.buttonInfo.name = "Home";
+    //         break;
+    //     }
+    //     case 9:
+    //     {
+    //         deviceInfo.buttonInfo.name = "LS";
+    //         break;
+    //     }
+    //     case 10:
+    //     {
+    //         deviceInfo.buttonInfo.name = "RS";
+    //         break;
+    //     }
+    //     default:
+    //     {
+    //         deviceInfo.buttonInfo.name = QString::number(button);
+    //         break;
+    //     }
+    //     }
+    // }
 }
--- ps5-screen/backend/gamepad/gamepadmanager.h
+++ ps5-screen/backend/gamepad/gamepadmanager.h
@@ -2,10 +2,40 @@
 #define GAMEPADMANAGER_H
 
 #include <QDebug>
+#include <QFuture>
+#include <QSharedPointer>
+#include <QtConcurrent/QtConcurrent>
 #include <QObject>
 #include <fcntl.h>
 #include <unistd.h>
 #include <linux/joystick.h>
+
+#define DEVICE_PATH "/dev/input/js0"
+
+enum class PSButton: uint8_t
+{
+    CROSS = 0,
+    CIRCLE,
+    SQUARE,
+    TRIANGLE,
+    COUNT,
+    UNKNOWN
+};
+
+inline QMap<uint8_t, QString> XboxButton
+    {
+        {   0,     "accept"     },
+        {   1,     "cancel"     },
+        {   2,     "X"          },
+        {   3,     "Y"          },
+        {   4,     "LB"         },
+        {   5,     "RB"         },
+        {   6,     "Back"       },
+        {   7,     "Start"      },
+        {   8,     "Xbox"       },
+        {   9,     "LS"         },
+        {   10,    "RS"         }
+    };
 
 class GamepadManager : public QObject
 {
@@ -15,10 +45,17 @@ public:
     explicit GamepadManager(QObject *parent = nullptr);
 
 private:
-    void run();
-    int readEvent(int fd, struct js_event *event);
+    static void handleXboxInput(int fd);
+    static int readEvent(int fd, js_event *event);
+
+    static void getButton(uint8_t button, int16_t value);
+
+    QFuture<void> future;
+
+    void test();
 
 signals:
+    void actionTriggered(QString name, QString state);
 };
 
 #endif // GAMEPADMANAGER_H
